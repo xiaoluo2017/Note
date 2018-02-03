f8f6c68d10d58e330ddec304213f73f059b635ef
Xiao Luo <xiao2017luo@gmail.com>
2018-02-03 13:22:35 -0800

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -8,3 +8,25 @@
         printf("%d", language);
     #endif 
 #endif 
+
+// Question 5.A
+class Dog {
+    private:
+    string type;
+    
+    public:
+    Dog(string type) {
+        this->type = type;
+    }
+    
+    string getType() {
+        return type;
+    }
+};
+
+int main()
+{
+    #define getType(x) x.getType()
+    Dog d("Black");
+    cout <<getType(d) << endl;
+}
