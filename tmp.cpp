bfca6d6bef56b244634f292c61af9939fea9aa2f
Xiao Luo <xiao2017luo@gmail.com>
2018-02-03 16:38:29 -0800

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -44,3 +44,53 @@ Container Adapters: <stack>, <queue>(including priority_queue)
 
 reference: https://stackoverflow.com/questions/3873802/what-are-containers-adapters-c
 */
+
+// Question 7
+/*
+istream& getline (char* s, streamsize n );
+istream& getline (char* s, streamsize n, char delim );
+
+reference: http://www.cplusplus.com/reference/istream/istream/getline/
+*/
+
+// Question 8
+class Car{
+ 
+public:
+	Car(){
+		cout << "Car's Constructor\n";
+	}
+ 
+	Car(const Car &ob){ // copy constructor
+		cout << "Car's Copy Constructor\n";
+	}
+ 
+	//Function that will receive object by value and return object by value.
+ 
+	Car function(Car ob/*call copy constructor*/){
+ 
+		//do something...
+		
+		//When a function returns an object of that class by value.
+		return ob;// call copy constructor
+	}
+ 
+};
+ 
+ 
+int main()
+{	
+	Car obj1; //call normal constructor
+	
+	Car obj2 = obj1; // call copy constructor 
+	
+	/*
+	When an object of the same class type is passed by value
+	as a parameter to a function.
+	function returns an object by value
+	*/
+	Car obj3 = obj1.function(obj1);
+ 
+ 
+	return 0;
+}
